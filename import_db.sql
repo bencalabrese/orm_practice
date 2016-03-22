eee5ad0f18da7d2862c69adbfb5122c05f53c7a4
Ben Calabrese <bencalabrese@gmail.com>
2016-03-22 11:42:50 -0700

    refactored work and added seed values

--- import_db.sql
+++ import_db.sql
@@ -1,8 +1,12 @@
+DROP TABLE IF EXISTS users;
+
 CREATE TABLE users (
   id INTEGER PRIMARY KEY,
   fname VARCHAR(255) NOT NULL,
   lname VARCHAR(255) NOT NULL
 );
+
+DROP TABLE IF EXISTS questions;
 
 CREATE TABLE questions (
   id INTEGER PRIMARY KEY,
@@ -13,37 +17,53 @@ CREATE TABLE questions (
   FOREIGN KEY (author_id) REFERENCES users(id)
 );
 
+DROP TABLE IF EXISTS question_follows;
+
 CREATE TABLE question_follows (
   id INTEGER PRIMARY KEY,
 
   user_id INTEGER NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users(id),
-
   question_id INTEGER NOT NULL,
+
+  FOREIGN KEY (user_id) REFERENCES users(id),
   FOREIGN KEY (question_id) REFERENCES questions(id)
 );
+
+DROP TABLE IF EXISTS replies;
 
 CREATE TABLE replies (
   id INTEGER PRIMARY KEY,
 
   question_id INTEGER NOT NULL,
-  FOREIGN KEY (question_id) REFERENCES questions(id),
-
-  parent_id INTEGER,
-  FOREIGN KEY (parent_id) REFERENCES replies(id),
-
   user_id INTEGER NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users(id),
+  parent_id INTEGER,
+  body TEXT NOT NULL,
 
-  body TEXT NOT NULL
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES questions(id),
+  FOREIGN KEY (parent_id) REFERENCES replies(id)
 );
+
+DROP TABLE IF EXISTS question_likes;
 
 CREATE TABLE question_likes (
   id INTEGER PRIMARY KEY,
 
   user_id INTEGER NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users(id)
-
   question_id INTEGER NOT NULL,
-  FOREIGN KEY (quesiton_id) REFERENCES questions(id)
+
+  FOREIGN KEY (question_id) REFERENCES questions(id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
+
+
+INSERT INTO
+  users (fname, lname)
+VALUES
+  ('John','Doe');
+
+INSERT INTO
+  questions (title, body, author_id)
+VALUES
+  ('Does this work', 'yayayaya', 1);
