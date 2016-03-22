84373f0634b5b225e6e1feb9a4bc88bd0099ce70
Ben Calabrese <bencalabrese@gmail.com>
2016-03-22 11:17:44 -0700

    Created questions table

--- import_db.sql
+++ import_db.sql
@@ -3,3 +3,12 @@ CREATE TABLE users (
   fname VARCHAR(255) NOT NULL,
   lname VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE questions (
+  id INTEGER PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  body VARCHAR(255) NOT NULL,
+  author_id INTEGER NOT NULL,
+
+  FOREIGN KEY (author_id) REFERENCES users(id)
+);
