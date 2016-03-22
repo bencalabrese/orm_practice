97d01ce41a40cff04a94e97acfc6e7a891ed989e
Ben Calabrese <bencalabrese@gmail.com>
2016-03-22 12:17:52 -0700

    added User::find_by_id

--- orm.rb
+++ orm.rb
@@ -16,14 +16,27 @@ class User
   attr_accessor :fname, :lname
 
   def self.all
-    data = QuestionsDatabase.instance.execute (<<-SQL)
+    data = QuestionsDatabase.instance.execute(<<-SQL)
       SELECT
         *
       FROM
         users
-      SQL
+    SQL
 
     data.map { |datum| User.new(datum) }
+  end
+
+  def self.find_by_id(id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        users
+      WHERE
+        id = ?
+    SQL
+
+    User.new(data.first)
   end
 
   def initialize(opts)
