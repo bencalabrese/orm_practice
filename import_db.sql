23221176f6b29093dee7a5ec601c10f6984ec9e4
Ben Calabrese <bencalabrese@gmail.com>
2016-03-22 11:26:10 -0700

    CREATED REPLIES TABLE AND QUESTIONS FOLLOW TABLE

--- import_db.sql
+++ import_db.sql
@@ -12,3 +12,28 @@ CREATE TABLE questions (
 
   FOREIGN KEY (author_id) REFERENCES users(id)
 );
+
+CREATE TABLE question_follows (
+  id INTEGER PRIMARY KEY,
+
+  user_id INTEGER NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+
+  question_id INTEGER NOT NULL,
+  FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+CREATE TABLE replies (
+  id INTEGER PRIMARY KEY,
+  
+  question_id INTEGER NOT NULL,
+  FOREIGN KEY (question_id) REFERENCES questions(id),
+
+  parent_id INTEGER NOT NULL,
+  FOREIGN KEY (parent_id) REFERENCES replies(id),
+
+  user_id INTEGER NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+
+  body
+)
