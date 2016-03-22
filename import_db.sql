467a18acd74884ce907c5664282948a546eb9873
Ben Calabrese <bencalabrese@gmail.com>
2016-03-22 11:29:17 -0700

    Questions_likes table

--- import_db.sql
+++ import_db.sql
@@ -7,7 +7,7 @@ CREATE TABLE users (
 CREATE TABLE questions (
   id INTEGER PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
-  body VARCHAR(255) NOT NULL,
+  body TEXT NOT NULL,
   author_id INTEGER NOT NULL,
 
   FOREIGN KEY (author_id) REFERENCES users(id)
@@ -25,15 +25,25 @@ CREATE TABLE question_follows (
 
 CREATE TABLE replies (
   id INTEGER PRIMARY KEY,
-  
+
   question_id INTEGER NOT NULL,
   FOREIGN KEY (question_id) REFERENCES questions(id),
 
-  parent_id INTEGER NOT NULL,
+  parent_id INTEGER,
   FOREIGN KEY (parent_id) REFERENCES replies(id),
 
   user_id INTEGER NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users(id),
 
-  body
-)
+  body TEXT NOT NULL
+);
+
+CREATE TABLE question_likes (
+  id INTEGER PRIMARY KEY,
+
+  user_id INTEGER NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+
+  question_id INTEGER NOT NULL,
+  FOREIGN KEY (quesiton_id) REFERENCES questions(id)
+);
